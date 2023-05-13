0784e910b5d256d20df91480218821be1e0f3bf6
ndmkcn <ndmkcn1453@gmail.com>
2023-05-13 15:48:01 +0300

    Stafflar listelendi ve commit yapıldı.

--- Frontend/HotelProject.WebUI/Controllers/StaffController.cs
+++ Frontend/HotelProject.WebUI/Controllers/StaffController.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Models.Staff;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class StaffController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _httpClient;
+        public StaffController(HttpClient httpClient)
         {
+            _httpClient = httpClient;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var responseMessage = await _httpClient.GetAsync("http://localhost:5121/api/Staff");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<List<StaffViewModel>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
--- Frontend/HotelProject.WebUI/Program.cs
+++ Frontend/HotelProject.WebUI/Program.cs
@@ -1,7 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Bunu http isteklerini kullanabilmek için ilgili arayüzün dependency injection ile inject edilmesini sağlamak içindir.
+/*
+ * AddHttpClient() metodu, HttpClient sınıfının Dependency Injection (DI) tarafından yönetilmesini sağlar. 
+ * Bu yöntem, HttpClient kullanarak uzak bir kaynağa (örneğin, bir API'ya) HTTP istekleri göndermek için 
+ * kullanılan bir HttpMessageHandler örneği oluşturur ve yapılandırır. 
+ * Bu metot aynı zamanda, IHttpClientFactory arayüzü üzerinden HttpClient örneği oluşturmak için de kullanılır. 
+ * Bu, HttpClient örneği oluşturma ve yönetim işlemlerini kolaylaştırır. Örneğin, HttpClient nesnelerinin çok sayıda 
+ * istemci tarafından paylaşılmasını kolaylaştırır ve bunları özelleştirmek için daha kolay bir yol sağlar. 
+ */
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
